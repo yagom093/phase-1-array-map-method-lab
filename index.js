a1fbc9fa7efc8175ba2e663ea98dc582ad975e52
yago <yagom10@hotmail.com>
2023-04-15 00:41:42 -0700

    complete

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+return tutorials
+.map(tutorial => {
+  let words = tutorial.split(' ')
+  let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  let response = capitalizedWords.join(' ')
+  return response
+})
 }
